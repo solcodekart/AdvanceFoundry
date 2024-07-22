ef55f55aabe34fe002c16cf78a1bd64f84f29399
ivan <ivanvk997@gmail.com>
2024-07-22 22:00:30 +0300

    DEFi project

---
+++ README.md
@@ -0,0 +1,8 @@
+1. (Relative Stability) Anchored or Pegged -> $1.00
+   1. Chainlink Price Feed
+   2. Set a function to exchange ETH & BTC -> $$$
+2. Stability Mechanism (Minting): Algorithmic (Decentralized)
+   1. People can only mint a stablecoin with enough collateral (coded)
+3. Collateral: Exogenus (Crypto)
+    1.wETH
+    2.wBTC
---
+++ test/fuzz/OpenInvariantsTest.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+//  pragma solidity 0.8.26;
+
+// import {Test, console} from "forge-std/Test.sol";
+// import {StdInvariant} from "forge-std/StdInvariant.sol";
+// import {DeployDSC} from "../../script/DeployDSC.s.sol";
+// import {DSCEngine} from "../../src/DSCEngine.sol";
+// import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+// import {HelperConfig} from "../../script/HelperConfig.s.sol";
+// import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// contract InvariantsTest is StdInvariant, Test {
+//     DeployDSC deployer;
+//     DSCEngine dsce;
+//     DecentralizedStableCoin dsc;
+//     HelperConfig config;
+//     address weth;
+//     address wbtc;
+
+//     function setUp() public {
+//         deployer = new DeployDSC();
+//         (dsc, dsce, config) = deployer.run();
+//         (,, weth, wbtc,) = config.activeNetworkConfig();
+//         targetContract(address(dsce));
+//     }
+
+//     function invariant_protocolMustHaveMoreValueThatTotalSupplyDollars() public view {
+//         uint256 totalSupply = dsc.totalSupply();
+//         uint256 wethDeposted = IERC20(weth).balanceOf(address(dsce));
+//         uint256 wbtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
+
+//         uint256 wethValue = dsce.getUsdValue(weth, wethDeposted);
+//         uint256 wbtcValue = dsce.getUsdValue(wbtc, wbtcDeposited);
+
+//         console.log("wethValue: %s", wethValue);
+//         console.log("wbtcValue: %s", wbtcValue);
+
+//         assert(wethValue + wbtcValue >= totalSupply);
+//     }
+// }
